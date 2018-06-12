a556a683d0b9d67742ab89a2820951b9ee697901
kate <kate_173yk@outlook.jp>
2018-06-12 16:49:10 +0900

    this select your fortune

---
+++ omikuzi.js
@@ -0,0 +1,23 @@
+(function(){
+  'use strict';
+
+  var btn = document.getElementById('btn');
+
+  btn.addEventListener('click',function(){
+    var n = Math.random();
+    if (n < 0.05){
+      this.textContent = "大吉"
+    }else if(n < 0.2){
+      this.textContent = "中吉"
+    }else{
+      this.textContent = "凶"
+    }
+    });
+    
+  btn.addEventListener('mousedown',function(){
+    this.className = 'pushed';
+  })
+  btn.addEventListener('mouseup',function(){
+    this.className = '';
+  })
+})();
